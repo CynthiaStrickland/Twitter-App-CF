68ec15caf76686cdc218e131479254a5a445dc1c
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-26 13:50:08 -0700

    TableViews

---
+++ Twitter App/TweetJSON.swift
@@ -0,0 +1,44 @@
+//
+//  TweetJSON.swift
+//  Twitter App
+//
+//  Created by Cynthia Whitlatch on 10/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import Foundation
+
+class TweetJSON {
+    
+    class func tweetFromJSONData(json: NSData) -> [Tweet]? {
+    
+    do {
+        
+         // Can user successfully translate the JSON data
+        if let rootObject = try NSJSONSerialization.JSONObjectWithData(json, options: NSJSONReadingOptions.MutableContainers) as? [[String : AnyObject]] {
+            
+            //create / initialize an array
+            var tweets = [Tweet]()
+            
+            //enumerate through the array
+                for tweetObject in rootObject {
+                    //if it has values
+                    if let text = tweetObject["name"] as? String, id = tweetObject["id_str"] as? String {
+                        //initialize it and append it to the array
+                        let tweet = Tweet(text: text, id: id)
+                        tweets.append(tweet)
+                }
+            }
+                return tweets
+                
+        }
+            
+    } catch _ {
+
+            
+        }
+        return nil
+        
+    
+    }
+}
--- Twitter App/TweetViewController.swift
+++ Twitter App/TweetViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var tweets: String?
+   
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+
+    class func identifier() -> String {
+        return "TweetViewController"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+        
+        
     }
-
+    
+    //MARK:  TABLEVIEW METHODS
+    
+    
+    func setUpTableView() {
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+     
+        return cell
+        
+    }
+    
 }
 
