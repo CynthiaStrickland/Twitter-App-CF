b0ac5c98a736ce5005f8d2785523f2cfa437c817
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-26 14:03:10 -0700

    Twitter App TableView

---
+++ Twitter App/Tweet.swift
@@ -0,0 +1,23 @@
+//
+//  Tweet.swift
+//  Twitter App
+//
+//  Created by Cynthia Whitlatch on 10/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    
+    let text: String
+    let id: String
+    
+    init(text: String, id: String) {
+        self.text = text
+        self.id = id
+    }
+    
+}
+
+
