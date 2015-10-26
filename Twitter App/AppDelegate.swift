fc4a03501fd917098786969b7f9f4f05c91e5ede
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-26 13:14:14 -0700

    Twitter App set-up

---
+++ Twitter App/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Twitter App
+//
+//  Created by Cynthia Whitlatch on 10/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        return true
+    }
+
+}
+
---
+++ Twitter App/TweetViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TweetViewController.swift
+//  Twitter App
+//
+//  Created by Cynthia Whitlatch on 10/26/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
